7d8f5dd6d3bcc0bdfec4c18eaba71b0a903748c7
abdemeh <abdellatif.elmahdaoui@gmail.com>
2024-07-04 15:41:12 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,34 @@
+// main.js
+
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    resizable: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  mainWindow.loadFile('index.html');
+  mainWindow.setMenu(null);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
